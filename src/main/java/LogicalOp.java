ef378989fe7db7100cacda58caa1006a1ae6bbef
MarilenaCretu <marilenac2001@gmail.com>
2022-10-22 17:14:49 +0300

    commit2

--- src/main/java/LogicalOp.java
+++ src/main/java/LogicalOp.java
@@ -51,12 +51,40 @@ public class LogicalOp {
     }
 
     //exercitiul 8
-    public String numberswich(int number) {
+    public int numberswich(int number) {
         switch (number) {
             case 1:
+                System.out.println("The number is: 1!");
+                break;
+            case 2:
+                System.out.println("The number is: 2!");
+                break;
+            case 3:
+                System.out.println("The number is: 3!");
+                break;
+            case 4:
+                System.out.println("The number is: 4!");
+                break;
+            case 5:
+                System.out.println("The number is: 5!");
+                break;
+            case 6:
+                System.out.println("The number is: 6!");
+                break;
+            case 7:
+                System.out.println("The number is: 7!");
+                break;
+            case 8:
+                System.out.println("The number is: 8!");
+                break;
+            case 9:
+                System.out.println("The number is: 9!");
+                break;
+            case 10:
+                System.out.println("The number is: 10!");
                 break;
         }
-        return "The number is: " + number + "!";
+        return number;
     }
 
     //exercitiul 9
