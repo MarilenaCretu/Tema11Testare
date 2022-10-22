bff606daffe84d128efd58d524d040934888c33c
MarilenaCretu <marilenac2001@gmail.com>
2022-10-22 13:15:50 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator=new Calculator();
+        //System.out.println(calculator.getModelfata());
+
+        System.out.println(calculator.getExercitiulunu());
+
+        System.out.println(calculator.getAfisarecerintadoi());
+        System.out.println(calculator.getPrimmulnumar());
+        System.out.println(calculator.getAldoileanumar());
+        System.out.println(calculator.getExercitiuldoi());
+
+        System.out.println(calculator.getExercitiultrei());
+
+        System.out.println(calculator.getCalculA());
+        System.out.println(calculator.getCalculB());
+        System.out.println(calculator.getCalculC());
+        System.out.println(calculator.getCalculD());
+
+        System.out.println(calculator.getSuma());
+        System.out.println(calculator.getScaderea());
+        System.out.println(calculator.getInmultirea());
+        System.out.println(calculator.getImpartirea());
+
+        System.out.println(calculator.getJava());
+        System.out.println(calculator.getMedia());
+        System.out.println(calculator.getModelfata());
+        System.out.println(calculator.getRestulimpartirii());
+        System.out.println(calculator.getTemperatura());
+        System.out.println(calculator.getMetri());
+        System.out.println(calculator.getDistanta());
+    }
 }
